bcfb6edec9cd307e439d9fa68a99a2f7c1fac5f0
canel007 <cguillermo007@hotmail.com>
2013-08-13 13:19:43 -0600

    parte inicial MDI

---
+++ MDI/MDI/prueba.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MDI
+{
+    public partial class frmprueba : Form
+    {
+        public frmprueba()
+        {
+            InitializeComponent();
+        }
+
+        private void frmprueba_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
