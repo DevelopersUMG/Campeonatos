7bc9fb5e4c549768a3d6c0822f245241b81af84d
Luis Ramos <guicholarg@hotmail.com>
2013-08-13 18:12:20 -0600

    Proyecto para iniciar
    Agregue una carpeta de proyectos temporales para poder subir forms de
    prueba y el proyecto SLN donde implementaremos todo

---
+++ Campeonatos/Campeonatos/Bienvenida.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Campeonatos
+{
+    partial class form_bienvenida
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // form_bienvenida
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 641);
+            this.Name = "form_bienvenida";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bienvenido";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Campeonatos/Campeonatos/Bienvenida.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Campeonatos
+{
+    public partial class form_bienvenida : Form
+    {
+        public form_bienvenida()
+        {
+            InitializeComponent();
+        }
+    }
+}
